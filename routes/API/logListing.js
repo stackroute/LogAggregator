3d8a2a5dadaf81ec89bda92fa6275fd3ccdfdc2f
unknown <SH324240@L-777726105.wipro.com>
2015-12-23 10:51:56 +0530

    done

--- routes/API/logListing.js
+++ routes/API/logListing.js
@@ -7,16 +7,19 @@ router.get('/:href', function(req, res  ) {
    var href = req.params.href;
 
    var filename = "path"+href+".json";
+
    var file = fs.readFileSync('json/logListing/'+filename, 'utf-8');
+
+   //res.download("path1.json");
    res.send(file);
 
 });
 
 
-router.get('/All', function(req, res) {
-  var file = fs.readFileSync('json/logListing/pathall.json','utf-8');
-  res.send(file);
-});
+// router.get('/All', function(req, res) {
+//   var file = fs.readFileSync('json/logListing/pathall.json','utf-8');
+//   res.send(file);
+// });
 
 
 router.get('/', function(req, res ) {
