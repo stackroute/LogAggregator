5cd6d2c2eb8b98012b0e37d31fa6a3caeef7b45b
unknown <GA324255@L-777726122.wipro.com>
2016-01-07 17:21:20 +0530

    Adding bower.json,.bowercc and public(folder)

---
+++ public/application.js
@@ -0,0 +1,13 @@
+var mainApplicationModuleName = 'logAggregator';
+
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute']);
+
+mainApplicationModule.config(['$locationProvider',
+  function($locationProvider) {
+    $locationProvider.hashPrefix('!');
+  }
+]);
+
+angular.element(document).ready(function() {
+  angular.bootstrap(document, [mainApplicationModuleName]);
+});
---
+++ public/main/controllers/logAggregator.controller.js
@@ -0,0 +1,5 @@
+angular.module('logAggregator').controller('ExampleController', ['$scope',
+  function($scope) {
+    $scope.name = 'MEAN Application';
+  }
+]);
