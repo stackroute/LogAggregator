b35202c97d4340a109b8c7cd78b8abba9de8e9bc
prateekreddy <prateekreddyy@gmail.com>
2016-01-08 11:08:05 +0530

    changes to project structure

--- public/js/logAggregator.routes.js
+++ public/js/logAggregator.routes.js
@@ -2,16 +2,16 @@ angular.module('logAggregator').config(['$routeProvider',
   function($routeProvider) {
     $routeProvider
     .when('/', {
-      templateUrl: 'logListing/views/logListing.view.html'
+      templateUrl: 'nginx/logListing/views/logListing.view.html'
     })
     .when('/userAgent', {
-      templateUrl: 'userAgent/views/userAgent.view.html'
+      templateUrl: 'nginx/userAgent/views/userAgent.view.html'
     })
     .when('/logListing', {
-      templateUrl: 'logListing/views/logListing.view.html'
+      templateUrl: 'nginx/logListing/views/logListing.view.html'
     })
     .when('/trafficRate', {
-      templateUrl: 'trafficRate/views/trafficRate.view.html'
+      templateUrl: 'nginx/trafficRate/views/trafficRate.view.html'
     })
     .otherwise({
       redirectTo: '/'
