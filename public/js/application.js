81702b7bc32378bf6a56cc7c5c34a543fcb57a38
unknown <SH324240@L-777726105.wipro.com>
2016-01-08 12:06:45 +0530

    ll pg started with ang

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,6 @@
 var mainApplicationModuleName = 'logAggregator';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute','logListing']);
 
 mainApplicationModule.config(['$locationProvider',
   function($locationProvider) {
--- public/js/nginx/logListing/controllers/logListing.controller.js
+++ public/js/nginx/logListing/controllers/logListing.controller.js
@@ -1,9 +1,28 @@
 angular.module('logListing')
 
     .controller('pathsControl', ['$scope',
-      function($scope) {
-        $scope.name = 'MEAN Application';
-      }
+      function($scope,'pathsArray') {
+
+            pathsArray.load().success(function(data){
+                    var no_of_paths=10,
+                    no_of_paths = config.paths;
+                    paths_selected=[];
+                    data = JSON.parse(data);
+
+                    for(var i=0; i<no_of_paths; i++)
+                    {
+                      //storing paths into an array
+
+                        paths_selected.push({"path": data.arr[i]["path"],
+                        "count": data.arr[i]["count"]
+                        });
+                    }
+          });
+
+          $scope.paths= paths_selected;
+
+      };
+
     ]);
 
     .controller('tableControl', ['$scope',
