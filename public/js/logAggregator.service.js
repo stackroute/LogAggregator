88b305878d9e043fe9c4f2152d29caee642c4b8d
unknown <GA324255@L-777726122.wipro.com>
2016-01-08 16:25:17 +0530

    minor changes in logAggregrator.service.js

--- public/js/logAggregator.service.js
+++ public/js/logAggregator.service.js
@@ -1,15 +1,9 @@
 angular.module('logAggregator').factory('loadConfig', ['$http',
   function($http) {
     var factory={};
-    var config;
     factory.getdata=function(success){
-      if(config){
-        success(config);
-        return;
-      }
       $http.get('/config').success(function(data){
-        config=data;
-        success(config);
+        success(data);
       });
     };
     return factory;
