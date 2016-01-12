d08d3fd7fb9ff55675b99d4b5ab6ad90dce2337c
unknown <DE324257@L-777726110.wipro.com>
2016-01-12 14:39:12 +0530

    added trafficrate folder

---
+++ public/js/nginx/trafficRate/services/trafficRate.service.js
@@ -0,0 +1,13 @@
+
+angular.module('logAggregator').factory('getTrafficData',
+  function($http) {
+    return {
+      getData: function( year, month) {
+          return  $http({
+                method: "get",
+                url: "json/trafficRate/"+year+"/"+month,
+              });
+        }
+      }
+  }
+);
