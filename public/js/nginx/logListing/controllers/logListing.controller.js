6563aaa15525882039b230c42c7b5282aba4e988
unknown <SH324240@L-777726105.wipro.com>
2016-01-12 14:16:36 +0530

    filter file added

--- public/js/nginx/logListing/controllers/logListing.controller.js
+++ public/js/nginx/logListing/controllers/logListing.controller.js
@@ -12,96 +12,36 @@ angular.module('logAggregator')
                     //console.log(data.arr);
                     $scope.paths_selected = data.arr;
 
+             });//close then
 
+            $scope.pathClickEvent = function(obj){
 
-                         });//close then
-                $scope.pathClickEvent = function(obj){
-                  //console.log(obj.path);
+                  currentpath = (obj=="All") ? "All": obj.path;
+                  $scope.currentpath =  currentpath;
+                  logService.dispObj(currentpath,1).then(function(response){
 
-                    currentpath = (obj=="All") ? "All": obj.path;console.log("curpath "+currentpath);
-                          logService.dispObj(currentpath,1).then(function(response){
                               count = response.data.count;
                               $scope.view=(obj=="All") ? "All":"path";
-                              console.log(response.data);
-                              console.log("count-->  "+count);
                               $scope.clickedObj=response.data.collection_data;
                               $scope.numPages = (parseInt(count/100) + 1) ;
-                              console.log("fdfd---> "+$scope.numPages);
-                                $scope.getPgs = function(num){
+                              $scope.getPgs = function(num){
                                     return new Array(num);
                                   }
 
-                          });
+                  });
 
+              };//close click event
+              $scope.pathClickEvent("All");
 
-                        // //console.log($scope.getPgs(3));
-                        // $scope.myFilter = function(value) {
-                        //        console.log('filter '+ value.size);
-                        //        return (value.size.charAt('1'));
-                        //     };
-                  };//close click event
-                $scope.pagenoClickEvent = function(pgno){
-                      logService.dispObj(currentpath,pgno).then(function(response){
+              $scope.pagenoClickEvent = function(pgno){
+                    $scope.currentpage = pgno;
+                    logService.dispObj(currentpath,pgno).then(function(response){
                         $scope.clickedObj=response.data.collection_data;
-                });
+                    });
               }
 
 
 
+    }//close fn
 
-
-
-        }//close fn
-
-    ]);
-
-angular.module('logAggregator')
-          .filter('sizeFilter',function(){
-
-            return function(input){
-
-
-              size=parseInt(input)/8;
-              if(size>1000 && size<1000000)
-
-                  {size = parseFloat(size)/1000;
-                   size = (size).toFixed(2)+ " KB" ;}
-
-              else if (size<1000 ) {
-                   size = size + " B" ;}
-
-              else if (size>1000000 ) {
-                 size = parseFloat(size)/1000000;
-                 size = (size).toFixed(2)+ " MB" ;
-               }
-              return size;
-            }
-
-          });
-
-          angular.module('logAggregator')
-                    .filter('agentFilter',function(){
-
-                      return function(input){
-
-                          temp = input;
-                          title="";
-                          if(temp.indexOf("Chrome") != -1)
-                              {title="Chrome";}
-                          else if (temp.indexOf("Safari")!= -1)
-                              {title="Safari";}
-                          else if (temp.indexOf("Trident")!= -1)
-                              {title="IE";}
-                          else if (temp.indexOf("Vagrant")!= -1)
-                              {title="Others";}
-                          else if (temp.indexOf("Opr")!= -1)
-                               {title="Opera";}
-                          else if (temp.indexOf("-")!= -1)
-                              {title="Others";}
-                          else
-                                {title="Mozilla";}
-
-
-                          return title;
-                      }
-          });
+]);
--- public/js/nginx/logListing/services/logServices.js
+++ public/js/nginx/logListing/services/logServices.js
@@ -1,9 +1,8 @@
-console.log("services start");
 var mainApp = angular.module("logAggregator");
 mainApp.factory('logService',function($http){
 
           var data={};
-          var service = {
+    var service = {
           disparray: function(){
                         var promise = $http(
                                           {method:'GET', url:"/json/logListing/"}
@@ -12,34 +11,35 @@ mainApp.factory('logService',function($http){
 
 
                                               });
-                            return promise;
-                        },//close disparray
+                        return promise;
+          },//close disparray
 
-            dispObj: function(path,pgno){
-                    console.log("type   "+  typeof path);
-                  if(path!="All"){
-                    if(path.indexOf("%")!=-1)
-                    path = path.replace('%','/');
+          dispObj: function(path,pgno){
 
-                    temp =  (path!='/') ? path.substring(1):path;
-                    path = (path!='/') ? ('/'+encodeURIComponent(temp)):path;
+                if(path!="All"){
+                  if(path.indexOf("%")!=-1)
+                  path = path.replace('%','/');
 
-                    }
-                  else {
-                      path="/All"
-                  }    //console.log(path);
+                  temp =  (path!='/') ? path.substring(1):path;
+                  path = (path!='/') ? ('/'+encodeURIComponent(temp)):path;
 
-                      pageno = (pgno != "1") ? pgno:"1";
-                      console.log("/json/logListing"+path+"/"+pageno);
-                      var promise = $http(
-                                            {method:'GET', url:"/json/logListing"+path+"/"+pageno}
-                                          )
-                                      .success(function (data, status, headers, config) {
-                                              });
-                    return promise;
+                  }
+                else {
+                    path="/All"
+                }
 
-            }//close dispObj
+                pageno = (pgno != "1") ? pgno:"1";
+                var promise = $http(
+                                      {method:'GET', url:"/json/logListing"+path+"/"+pageno}
+                                    )
+                                .success(function (data, status, headers, config) {
+                                        });
 
-          }
+                return promise;
+
+          }//close dispObj
+
+    }//close service
           return service;
+
 });
