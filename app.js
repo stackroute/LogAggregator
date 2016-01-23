f09375159b59805cab22c08f79216632a033982e
deepak1745 <deepu1745@gmail.com>
2016-01-24 01:37:18 +0530

    changed login page background

--- app.js
+++ app.js
@@ -23,12 +23,7 @@ var userAgent = require('./routes/API/userAgent');
 var logListing = require('./routes/API/logListing');
 var trafficRate = require('./routes/API/trafficRate');
 
-// var signin = require('./routes/auth/signin');
-// var signout = require('./routes/auth/signout');
-// var signup = require('./routes/auth/signup')
-
 var app = express();
-//var passport = passport();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -61,10 +56,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/config', configRoute);
-
-// app.use('/signin', signin);
-// app.use('/signout', signout);
-// app.use('/signup', signup);
 var initPassport = require('./passport-init');
 initPassport(passport);
 app.use('/', routes);
